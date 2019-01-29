c15ddd1f50aefd06ee791c4a9672f9db473c2ef8
Dallas <djpillen@umich.edu>
2019-01-29 17:24:11 -0500

    move archivesspace directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,3 @@ apt-get -y update
 apt-get -y install automake bison git git-core curl zlib1g-dev build-essential libffi-dev \
         libgdbm-dev libssl-dev  libncurses5-dev libreadline-dev libyaml-dev libsqlite3-dev \
         libtool openjdk-8-jdk sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties unzip
-
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-apt-get install -y nodejs
--- setup_archivesspace.sh
+++ setup_archivesspace.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
 # Download and unzip a packaged ArchivesSpace release and corresponding source code
-if [ -d "/vagrant/aspace" ]; then
-  rm -rf /vagrant/aspace
+if [ -d "/aspace" ]; then
+  rm -rf /aspace
 fi
-mkdir /vagrant/aspace
-mkdir /vagrant/aspace/zips
-cd /vagrant/aspace/zips
+mkdir /aspace
+mkdir /aspace/zips
+cd /aspace/zips
 wget -nv https://github.com/archivesspace/archivesspace/releases/download/v2.5.2/archivesspace-v2.5.2.zip
-cd /vagrant/aspace
-unzip /vagrant/aspace/zips/archivesspace-v2.5.2.zip
+cd /aspace
+unzip /aspace/zips/archivesspace-v2.5.2.zip
 
 # These variables will be used to edit the ArchivesSpace config file to use the correct database URL
 DBURL='AppConfig[:db_url] = "jdbc:mysql://localhost:3306/archivesspace?user=as\&password=as123\&useUnicode=true\&characterEncoding=UTF-8"'
 
 # http://archivesspace.github.io/archivesspace/user/running-archivesspace-against-mysql/
-cd /vagrant/aspace/archivesspace/lib
+cd /aspace/archivesspace/lib
 wget -nv http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.47/mysql-connector-java-5.1.47.jar
 
 # Edit the config file to use the MySQL database
 # http://stackoverflow.com/questions/14643531/changing-contents-of-a-file-through-shell-script
-cd /vagrant/aspace/archivesspace/config
+cd /aspace/archivesspace/config
 sed -i "s@#AppConfig\[:db_url\].*@$DBURL@" config.rb
 
 # Make the archivesspace.sh and setup-database.sh scripts executable
-cd /vagrant/aspace/archivesspace/scripts
+cd /aspace/archivesspace/scripts
 chmod +x setup-database.sh
-cd /vagrant/aspace/archivesspace
+cd /aspace/archivesspace
 chmod +x archivesspace.sh
 
 # Run the setup database script
@@ -40,8 +40,8 @@ After=network.target
 [Service]
 Type=forking
 RemainAfterExit=yes
-ExecStart=/vagrant/aspace/archivesspace/archivesspace.sh start
-ExecStop=/vagrant/aspace/archivesspace/archivesspace.sh stop
+ExecStart=/aspace/archivesspace/archivesspace.sh start
+ExecStop=/aspace/archivesspace/archivesspace.sh stop
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/archivesspace.service
@@ -50,4 +50,4 @@ WantedBy=multi-user.target" > /etc/systemd/system/archivesspace.service
 systemctl start archivesspace.service
 systemctl enable archivesspace.service
 
-chown -R vagrant:vagrant /vagrant/aspace
+chown -R vagrant:vagrant /aspace
